ce23aaac9cc21456c590e6342926944a361c823c
leopoldkwok <leopoldkwok@gmail.com>
2018-02-05 21:42:06 +0000

    Data Types in JavaScript

---
+++ 01-JavaScript-Language-Fundamentals/04-Data-Types-in-JavaScript/js_sandbox/app.js
@@ -0,0 +1,27 @@
+// PRIMITIVE
+
+// String
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+// symbol
+const sym = Symbol();
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+// Object Literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+const today = new Date();
+
+console.log(today);
+console.log(typeof today);
