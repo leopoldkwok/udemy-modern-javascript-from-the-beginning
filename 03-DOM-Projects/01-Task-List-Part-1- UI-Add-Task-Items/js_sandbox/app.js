86b3140a1cfb27933a327160fbef1cd4afd076f0
leopoldkwok <leopoldkwok@gmail.com>
2018-03-05 23:46:01 +0000

    fixed console errors

--- 03-DOM-Projects/01-Task-List-Part-1- UI-Add-Task-Items/js_sandbox/app.js
+++ 03-DOM-Projects/01-Task-List-Part-1- UI-Add-Task-Items/js_sandbox/app.js
@@ -21,28 +21,27 @@ function addTask(e) {
   }
 
   // Create li element
-  const li = document.createElemen('li');
+  const li = document.createElement('li');
   // Add class
   li.className = 'collection-item';
   // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
-// Create new link element
-const link = document.createElement('a');
-// Add class
-link.className = 'delete-item secondary-content';
-// Add icon html
-link.innerHTML = '<i class="fa fa-remove"></i>';
-// Append the link to li
-li.appendChild(link);
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
 
-// Append li to ul
-taskList.appendChild(li);
+  // Append li to ul
+  taskList.appendChild(li);
 
-// Clear input
-taskInput.value = '';
+  // Clear input
+  taskInput.value = '';
 
-console.log(li);
+  console.log(li);
 
-e.preventDefault();
-
+  e.preventDefault();
 }
